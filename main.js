0058754cb309bb7f95dd8163cb737c5f033f6c7b
AashmannGarg <87020202+AashmannGarg@users.noreply.github.com>
2022-11-12 18:18:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //https://teachablemachine.withgoogle.com/models/O57moi-Rl/
 function startClassification() {
     navigator.mediaDevices.getUserMedia({ audio: true });
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/O57moi-Rl/', modelReady);
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/O57moi-Rl/model.json', modelReady);
 }
 function modelReady() {
     classifier.classify(gotResults);
@@ -19,24 +19,21 @@ function gotResults(error, results) {
         document.getElementById("result_confidence").innerHTML = "Accuracy-" + (results[0].confidence * 100).toFixed(2) + "%";
         document.getElementById("result_label").style.color = "rgb(" + random_number_R + "," + random_number_G + "," + random_number_B + ")";
         document.getElementById("result_confidence").style.color = "rgb(" + random_number_R + "," + random_number_G + "," + random_number_B + ")";
-        img1 = document.getElementById("alien1");
-        img2 = document.getElementById("alien2");
-        img3 = document.getElementById("alien3");
-        img4 = document.getElementById("alien4");
+        img1 = document.getElementById("image_animal");
         if (results[0].label == "Barking") {
-            img1.src="Dog 1";
+            img1.src="Dog1.png";
         }
         else if (results[0].label == "Mooing") {
-            img1.src="Cow 1";
+            img1.src="Cow1.jpg";
         }
         else if (results[0].label == "Meowing") {
-            img1.src = "Cat 1";
+            img1.src = "Cat1.jpg";
         }
         else if (results[0].label == "Roaring") {
-            img1.src = "Lion 1";
+            img1.src = "Lion1.jpg";
         }
         else {
-            img1.src = "Dog 1";
+            img1.src="";
         }
     }
 }
